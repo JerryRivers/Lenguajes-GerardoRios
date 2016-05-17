498c27dd8c565016ca4f53db0d1e5c04afaa16ac
T-107 <T-107@PC246>
2016-05-16 19:31:08 -0500

    Archivo actualizado con un segundo Print

--- src/lenguajes/gerardorios/LenguajesGerardoRios.java
+++ src/lenguajes/gerardorios/LenguajesGerardoRios.java
@@ -17,6 +17,7 @@ public class LenguajesGerardoRios {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Mi 1er programa de Lenguajes");
+        System.out.println("Esta es otra linea de codigo");
     }
     
 }
