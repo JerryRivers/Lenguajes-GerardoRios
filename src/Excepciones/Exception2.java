6d3e5e787233a00d8ead7f9dbadff0d594778e34
T-107 <T-107@PC246>
2016-06-09 19:41:56 -0500

    Excepciones-2da Parte

--- src/Excepciones/Exception2.java
+++ src/Excepciones/Exception2.java
@@ -21,15 +21,38 @@ public class Exception2
 {
     public static void main(String[] args) 
     {
-        File file=new File("E:\\Escuela\\Java2\\prueba.txt");
+        File file=new File("E:\\Escuela\\Java2\\prueba");
         try 
         {
+            int x=2;
+            int y=0;
+            int div=x/y;
+            
             FileInputStream fis=new FileInputStream(file); //FileInputStream
+            int ch=0;
+            StringBuilder builder=new StringBuilder();
+            while((ch=fis.read())!=-1)
+            {
+                builder.append((char)ch);
+            }
+            System.out.println("Se leyo bien y se encontró esto: "+builder.toString());
         }
-        catch (/*FileNotFound*/Exception ex) 
+        catch (FileNotFoundException ex) 
+        {
+            System.out.println("Uso de try catch 'FileNotFoundException ex', revisa el nombre de tu archivo");
+        } 
+        catch (IOException ex) 
+        {
+            System.out.println("Uso de try catch 'IOException ex'");
+        }
+        catch (Exception e)
+        {
+            System.out.println("Uso de try catch 'Exception e' "+e.getMessage());
+        }
+        finally //Finally siempre se ejecuta se cumplan o no las excepciones
         {
             
         }
-        System.out.println(file);
+       // System.out.println(file);
     }
 }
